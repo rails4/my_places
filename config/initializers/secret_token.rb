474e79e202f7bf984d642f7fcdb7060a5a944749
Wlodek Bzyl <matwb@univ.gda.pl>
2014-02-08 21:36:36 +0100

    pierwszy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyPlaces::Application.config.secret_key_base = '66ffdec2a48bdd2b75ba132f968ac064abb7a0b155de48c190b4fcbf631ad01e674804ed4512de836880234ad8d79c8f148ee6a466b0df46ab211db9bba749b0'
